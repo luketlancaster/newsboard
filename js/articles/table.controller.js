f4c08fde47d37c897e97e1fccc98ffe19478a70d
Luke Lancaster <luketlancaster@gmail.com>
2015-03-13 19:21:46 -0500

    prevent users from voting on the same item multiple times

--- js/articles/table.controller.js
+++ js/articles/table.controller.js
@@ -3,23 +3,37 @@ angular
   .controller('ArticleTableCtrl', ArticleTableCtrl);
 
 function ArticleTableCtrl($http) {
-  var vm = this;
+  var vm   = this,
+      user = 'annon';
 
   vm.vote = function (direction, uuid) {
-    var data = vm.articles[uuid];
+    var data       = vm.articles[uuid],
+        vote       = {};
+        vote[uuid] = direction;
 
-    if (direction === 'up') {
-      data.votes.up++;
-    } else if (direction === 'down') {
-      data.votes.down++;
+    if(!vm.usersVotes[uuid]) {
+      if (direction === 'up') {
+        data.votes.up++;
+      } else {
+        data.votes.down++;
+      }
+
+      vm.usersVotes[uuid] = vote;
+
+      $http.put('https://newsagg.firebaseio.com/articles/' + uuid + '.json', data);
+      $http.patch('https://newsagg.firebaseio.com/votes/' + user + '/.json', vote);
     }
-
-    $http.put('https://newsagg.firebaseio.com/articles/' + uuid + '.json', data);
   };
 
   $http
     .get('https://newsagg.firebaseio.com/articles/.json')
     .success(function (data){
       vm.articles = data;
+    });
+
+  $http
+    .get('https://newsagg.firebaseio.com/votes/' + user + '/.json')
+    .success(function (data) {
+      vm.usersVotes = data || {};
     });
 }
