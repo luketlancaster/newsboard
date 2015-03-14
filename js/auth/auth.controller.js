98ed9a9a651f6cd677482a8915307371c08e9ee2
Luke Lancaster <luketlancaster@gmail.com>
2015-03-13 22:03:12 -0500

    register function up and running

--- js/auth/auth.controller.js
+++ js/auth/auth.controller.js
@@ -2,21 +2,38 @@ angular
   .module('news')
   .controller('AuthController', AuthController);
 
-function AuthController (BASE_URL) {
+function AuthController (BASE_URL, $location, $scope) {
   var vm = this;
   vm.user = {};
 
   vm.login = function () {
     var fb = new Firebase(BASE_URL);
 
-    fb.authWithPassword(vm.user, function(error, auth) {
+    fb.authWithPassword(vm.user, function (error, authData) {
       if(error) {
         console.log('nope', error);
       } else {
-        console.log('yup', auth);
+        console.log('yup', authData);
+        $location.path('/');
+        $scope.$apply();
       }
     });
+  };
 
+  vm.register = function () {
+    var fb = new Firebase(BASE_URL);
 
+    fb.createUser(vm.user, function (err, authData) {
+      if (err && err.code === 'EMAIL_TAKEN') {
+        console.log('Error creating user', err);
+        vm.login();
+      } else if (err) {
+        console.log('Error creating user', err);
+      } else {
+        console.log('You did it! You are in!', authData);
+        $location.path('/');
+        $scope.$apply();
+      }
+    });
   };
 }
